136447e8a29f358c85b2d59f6ce50ffb977894fa
l1va <lev_matematik@tut.by>
2018-01-21 03:27:39 +0300

    gui updated

---
+++ connection_frame.py
@@ -0,0 +1,96 @@
+import tkinter as tk
+import subprocess
+import serial
+
+MATRIX_TYPE = "Matrix"
+
+
+class Connection:
+    def __init__(self, parent):
+        self.parent = parent
+        parent_frame = self.parent.get_frame()
+        self.size_frame = tk.Frame(parent_frame)
+        self.size_frame.grid(row=0)
+        self.model_frame = tk.Frame(parent_frame)
+        self.model_frame.grid(row=1)
+        self.connect_frame = tk.Frame(parent_frame)
+        self.connect_frame.grid(row=2)
+
+        self.ports_listbox = tk.Listbox(self.connect_frame)
+        self.cols_edit = tk.Entry(self.size_frame)
+        self.rows_edit = tk.Entry(self.size_frame)
+
+        self.model = []
+
+    def draw(self):
+
+        self.cols_edit.delete(0, tk.END)
+        self.cols_edit.insert(0, "4")
+        self.rows_edit.delete(0, tk.END)
+        self.rows_edit.insert(0, "3")
+        tk.Label(self.size_frame, text="Rows: ").grid(row=0)
+        self.rows_edit.grid(row=0, column=1)
+        tk.Label(self.size_frame, text="Columns: ").grid(row=1)
+        self.cols_edit.grid(row=1, column=1)
+        change_size_button = tk.Button(self.size_frame, text="change size", width=10, command=self.change_size())
+        change_size_button.grid(row=2)
+
+        self.change_size()()
+
+        available_serials = get_available_serials()
+        for i in range(len(available_serials)):
+            self.ports_listbox.insert(i + 1, available_serials[i])
+
+        tk.Label(self.connect_frame, text="Port: ").grid(row=0)
+        self.ports_listbox.grid(row=0, column=1)
+        connect_button = tk.Button(self.connect_frame, text="connect", width=10, command=self.connect())
+        connect_button.grid(row=1)
+
+    def change_size(self):
+        def f():
+            for child in self.model_frame.winfo_children():
+                child.destroy()
+            tk.Label(self.model_frame, text="Leave selected checkboxes \nwhere you have leds: ").grid(row=1)
+
+            rows_count = int(self.rows_edit.get())
+            cols_count = int(self.cols_edit.get())
+            self.model = []
+            start_row = 2
+            for i in range(rows_count):
+                self.model.append([1] * cols_count)
+                for j in range(cols_count):
+                    port = i * cols_count + j
+                    var = tk.IntVar(value=1)
+                    chb = tk.Checkbutton(self.model_frame, text=str(port), variable=var,
+                                         command=self.checkbox_changed(i, j, var))
+                    chb.grid(row=start_row + i, column=j+1)
+
+        return f
+
+    def connect(self):
+        def f():
+            com_port = self.ports_listbox.get(tk.ACTIVE)
+            ser = serial.Serial(com_port)
+            print("connected to :" + ser.name)
+
+            self.parent.on_connect(ser, self.model)
+            self.size_frame.destroy()
+            self.model_frame.destroy()
+            self.connect_frame.destroy()
+
+        return f
+
+    def checkbox_changed(self, i, j, var):
+        def f():
+            self.model[i][j] = var.get()
+
+        return f
+
+
+def get_available_serials():
+    ports_string = subprocess.check_output(['python', '-m', 'serial.tools.list_ports']).decode("utf-8").strip()
+    print("FOUND: " + ports_string)
+    if len(ports_string) == 0: # TODO: REMOVE ME: stub to debug without any ports
+        print("stubs used as no ports found")
+        return ["/dev/ttyUSB0", "/dev/ttyUSB1"]
+    return ports_string.split('\n')
--- main.py
+++ main.py
@@ -1,84 +1,57 @@
-from tkinter import *
-import subprocess
-import serial
-
-try:
-    ports_string = subprocess.check_output(['python', '-m', 'serial.tools.list_ports']).decode("utf-8")
-    print("FOUND: " + ports_string)
-    ports = ports_string.strip().split('\n')
-except Exception as e:
-    ports = ["/dev/ttyUSB0", "/dev/ttyUSB1"]  # TODO: REMOVE ME: stub to debug without any ports
-    print("stubs used as no ports found, err:" + str(e))
-
-ard_ports = [i for i in range(40)]  # TODO: implement possibility to skip busy portrs
-
-top = Tk()
-
-ports_list = Listbox(top)
-for i in range(len(ports)):
-    ports_list.insert(i + 1, ports[i])
-
-cols_edit = Entry(top)
-cols_edit.delete(0, END)
-cols_edit.insert(0, "4")
-rows_edit = Entry(top)
-rows_edit.delete(0, END)
-rows_edit.insert(0, "3")
-
-ser = None
+import tkinter as tk
+import connection_frame
 
 
-def checkbox_changed(port, var):
+class Main:
+    def __init__(self):
+        self.serial = None
+        self.top = tk.Tk()
+
+    def start(self):
+        connection_frame.Connection(self).draw()
+        self.top.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.top.mainloop()
+
+    def on_closing(self):
+        if self.serial is not None:
+            self.serial.close()
+        self.top.destroy()
+
+    def get_frame(self):
+        return self.top
+
+    def on_connect(self, ser, model):
+        self.serial = ser
+        ind = 0
+        start_row = 1
+        for i in range(len(model)):
+            for j in range(len(model[0])):
+                if model[i][j] == 1:
+                    var = tk.IntVar()
+                    chb = tk.Checkbutton(self.top, text=str(ind), variable=var,
+                                         command=checkbox_changed(ind, var, self.serial))
+                    chb.grid(row=start_row + i, column=j)
+                    ind += 1
+
+
+def checkbox_changed(port, var, ser):
     def f():
         to_send = str(port)
         if var.get() == 1:
-            to_send+="+"
+            to_send += "+"
             print("checked " + str(port))
         else:
-            to_send +="-"
+            to_send += "-"
             print("unchecked " + str(port))
-        to_send+="\n"
+        to_send += "\n"
         ser.write(to_send.encode("utf-8"))
         ser.flush()
 
     return f
 
 
-def connect():
-    global ser
-    rows_count = int(rows_edit.get())
-    cols_count = int(cols_edit.get())
-    com_port = ports_list.get(ACTIVE)
-    print("connect rows:" + str(rows_count) + " cols:" + str(cols_count) + " port:" + com_port)
-    ser = serial.Serial(com_port)
-    print("connected to :" + ser.name)
-
-    ind = 0
-    start_row = 4  # TODO: fix placing
-    for i in range(rows_count):
-        for j in range(cols_count):
-            port = ard_ports[ind]
-            var = IntVar()
-            chb = Checkbutton(top, text=str(port), variable=var, command=checkbox_changed(port, var))
-            chb.grid(row=start_row + i, column=j)
-            ind += 1
+def main():
+    Main().start()
 
 
-connect_button = Button(top, text="connect", width=10, command=connect)
-
-Label(top, text="Port: ").grid(row=0)
-ports_list.grid(row=0, column=1)
-Label(top, text="Rows: ").grid(row=1)
-Label(top, text="Columns: ").grid(row=2)
-rows_edit.grid(row=1, column=1)
-cols_edit.grid(row=2, column=1)
-connect_button.grid(row=3)
-
-
-def on_closing():
-    ser.close()
-    top.destroy()
-
-
-top.protocol("WM_DELETE_WINDOW", on_closing)
-top.mainloop()
+main()
